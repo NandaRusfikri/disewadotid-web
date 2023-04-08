f6336676fa587c7ea14d785d5e2d92a7b6130de3
NandaRusfikri <muhammad.rusfikri@visionet.co.id>
2023-04-08 13:27:19 +0700

    coba init state

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,13 +1,13 @@
 import {useRouter} from "next/router";
-import {useState} from "react";
-import {useLogin} from "../src/hooks/auth/useLogin";
+import React, { useState } from 'react';
+import {useLogin} from "@/src/hooks/auth/useLogin";
 import Navbar from "@/pages/navbar";
 import Link from "next/link";
 import Head from "next/head";
 
 
 const Register = () => {
-    const [email, setName] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const {login} = useLogin();
     const router = useRouter();
@@ -51,14 +51,18 @@ const Register = () => {
                                     <label htmlFor="email"
                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                                         email</label>
-                                    <input type="email" name="email" id="email"
+                                    <input  value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        type="email" name="email" id="email"
                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            placeholder="name@company.com" />
                                 </div>
                                 <div>
                                     <label htmlFor="password"
                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••"
+                                    <input value={password}
+                                           onChange={(e) => setPassword(e.target.value)}
+                                        type="password" name="password" id="password" placeholder="••••••••"
                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     />
                                 </div>
